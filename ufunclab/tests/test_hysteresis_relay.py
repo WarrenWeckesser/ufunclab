bb1dfb567a8c5cba150eba371a4c8b0de473bbf1
Warren Weckesser <warren.weckesser@gmail.com>
2021-02-21 14:46:46 -0500

    Fix hysteresis_relay unit tests.

--- ufunclab/tests/test_hysteresis_relay.py
+++ ufunclab/tests/test_hysteresis_relay.py
@@ -1,15 +1,17 @@
 import numpy as np
 from numpy.testing import assert_array_equal
-from ufunclab import deadzone
+from ufunclab import hysteresis_relay
 
 
-def test_all_zeros():
-    x = np.zeros(10)
-    y = deadzone(x, -1, 1)
-    assert_array_equal(y, x)
 
 
 def test_basic():
-    x = np.array([-4, -3, -2, -1, 0, 1, 2])
-    y = deadzone(x, -2.5, 1.0)
-    assert_array_equal(y, [-1.5, -0.5, 0.0, 0.0, 0.0, 0.0, 1.0])
+    x = np.array([-4, -3, -2, -1, 0, 1, 2, 1, 0, -2])
+    y = hysteresis_relay(x, -0.5, 0.5, -1, 1, -1)
+    assert_array_equal(y, [-1, -1, -1, -1, -1, 1, 1, 1, 1, -1])
+
+
+def test_init():
+    x = np.array([0, 0, 9, 0, -9, 9])
+    y = hysteresis_relay(x, -1, 1, -2, 2, -3)
+    assert_array_equal(y, [-3, -3, 2, 2, -2, 2])
