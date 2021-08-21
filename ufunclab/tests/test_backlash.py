535956350f33b9fe79ccae5b47b537f6d79aad2d
Warren Weckesser <warren.weckesser@gmail.com>
2021-08-21 12:07:19 -0400

    BUG: backlash didn't handle non-standard strides correctly.

--- ufunclab/tests/test_backlash.py
+++ ufunclab/tests/test_backlash.py
@@ -31,3 +31,22 @@ def test_initial_b():
     x = np.array([1.75, 2, 3, 2.9, -1])
     y = backlash(x, 2, 0.5)
     assert_array_equal(y, [0.75, 1, 2, 2, 0])
+
+
+def test_nonstandard_strides():
+    t = np.linspace(0, 5, 11).reshape(-1, 1)
+    y = (10*np.sin(t + [0, 1, np.pi/2])).T[::2]
+    # y has strides (16, 24)
+    # y.copy() has strides (88, 8)
+    y1 = backlash(y, 1, 1, axis=1)
+    y2 = backlash(y.copy(order='C'), 1, 1)
+    assert_array_equal(y1, y2)
+
+
+def test_out_nonstandard_strides():
+    x = np.array([1, 2, 0, 3, 4, 5])
+    base = np.zeros(2*len(x))
+    out = base[::2]
+    y1 = backlash(x, 1, 1)
+    y2 = backlash(x, 1, 1, out=out)
+    assert_array_equal(y1, y2)
