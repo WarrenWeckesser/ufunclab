04fa45894c94ae9af90a0fdf33ddf33155359439
Warren Weckesser <warren.weckesser@gmail.com>
2021-09-26 11:33:48 -0400

    MAINT: findfirst:
    * Use more templating to reduce the repeated code.
    * Expose the Python values Py_LT, Py_LE, etc., in the
      _findfirst module.

--- ufunclab/__init__.py
+++ ufunclab/__init__.py
@@ -4,7 +4,7 @@ NumPy ufuncs and utilities.
 
 from ._logfact import logfactorial
 from ._cross3 import cross3
-from ._findfirst import findfirst
+from ._findfirst import findfirst, _LT, _LE, _EQ, _NE, _GT, _GE
 from ._peaktopeak import peaktopeak
 from ._minmax import minmax, argminmax, min_argmin, max_argmax
 from ._means import gmean, hmean
@@ -19,15 +19,13 @@ from ._ufunc_inspector import ufunc_inspector
 import numpy as _np
 
 
-# XXX Maybe use enum.IntEnum for `op`?
-
 class op:
     """
     Allowed values for the `op` argument of `findfirst`.
     """
-    LT = _np.int8(0)
-    LE = _np.int8(1)
-    EQ = _np.int8(2)
-    NE = _np.int8(3)
-    GT = _np.int8(4)
-    GE = _np.int8(5)
+    LT = _np.int8(_LT)
+    LE = _np.int8(_LE)
+    EQ = _np.int8(_EQ)
+    NE = _np.int8(_NE)
+    GT = _np.int8(_GT)
+    GE = _np.int8(_GE)
