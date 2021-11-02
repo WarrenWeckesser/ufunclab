443543ffc3d6b296015e53e622d4f62846d13b19
warren <warren.weckesser@gmail.com>
2021-11-02 08:04:07 -0400

    MAINT: Increment dev level

--- setup.py
+++ setup.py
@@ -93,5 +93,5 @@ def configuration(parent_package='', top_path=None):
 if __name__ == "__main__":
     from numpy.distutils.core import setup
     setup(name='ufunclab',
-          version='0.0.5.dev6',
+          version='0.0.5.dev7',
           configuration=configuration)
