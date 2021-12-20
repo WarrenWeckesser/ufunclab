a28f82f3ee3243ea32375f223d69cae38c5dbd2f
warren <warren.weckesser@gmail.com>
2021-12-19 23:11:49 -0500

    More work on experimental wrapper generator

--- src/step/define_cxxgen_extmods.py
+++ src/step/define_cxxgen_extmods.py
@@ -2,33 +2,31 @@
 from ufunc_config_types import ExtMod, Func
 
 
-step_docstring = """
-step(x, a, flow, fa, fhigh)
+step_docstring = """\
+step(x, a, flow, fa, fhigh, /, ...)
 
 The function `step(x, a, flow, fa, fhigh)` returns `flow` for
 `x < a`, `fhigh` for `x > a`, and `fa` for `x = a`.
-
-The Heaviside function can be implemented as `step(x, 0, 0, 0.5, 1)`.
 """
 
-linearstep_docstring = """
-linearstep(x, a, b, fa, fb)
+linearstep_docstring = """\
+linearstep(x, a, b, fa, fb, /, ...)
 
 The function `linearstep(x, a, b, fa, fb)` returns `fa` for
 `x <= a`, `fb` for `x >= b`, and uses linear interpolation
-from `fa` to `fb` in the interval `a < x < b`. 
+from `fa` to `fb` in the interval `a < x < b`.
 """
 
-smoothstep3_docstring = """
-smoothstep3(x, a, b, fa, fb)
+smoothstep3_docstring = """\
+smoothstep3(x, a, b, fa, fb, /, ...)
 
 The function `smoothstep3(x, a, b, fa, fb)` returns `fa` for
 `x <= a`, `fb` for `x >= b`, and uses a cubic polynomial in
 the interval `a < x < b` to smoothly transition from `fa` to `fb`.
 """
 
-smoothstep5_docstring = """
-smoothstep5(x, a, b, fa, fb)
+smoothstep5_docstring = """\
+smoothstep5(x, a, b, fa, fb, /, ...)
 
 The function `smoothstep5(x, a, b, fa, fb)` returns `fa` for
 `x <= a`, `fb` for `x >= b`, and uses a degree 5 polynomial in
