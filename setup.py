bc320d404897bf88128a99ebadbbebea48b0b7c7
warren <warren.weckesser@gmail.com>
2022-03-06 15:05:05 -0500

    Add npymath lib to the build of the vnorm extension module.

--- setup.py
+++ setup.py
@@ -66,6 +66,10 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('ufunclab')
     config.add_subpackage('ufunclab/tests')
 
+    util_include_dir = join('src', 'util')
+    npymath_info = get_info("npymath")
+    npymath_info['include_dirs'].append(util_include_dir)
+
     config.add_extension('ufunclab._logfact',
                          extra_compile_args=compile_args,
                          sources=[join('src', 'logfactorial',
@@ -83,28 +87,28 @@ def configuration(parent_package='', top_path=None):
                          extra_compile_args=['-std=c++11', '-Werror'],
                          sources=[join('src', 'abs_squared', 'generated', name)
                                   for name in _as_srcs],
-                         **get_info("npymath"))
+                         **npymath_info)
 
     _ei_srcs = ['expint1_concrete.cxx', '_expint1module.cxx']
     config.add_extension('ufunclab._expint1',
                          extra_compile_args=['-std=c++11', '-Werror'],
                          sources=[join('src', 'expint1', 'generated', name)
                                   for name in _ei_srcs],
-                         **get_info("npymath"))
+                         **npymath_info)
 
     _le_srcs = ['logistic_concrete.cxx', '_logisticmodule.cxx']
     config.add_extension('ufunclab._logistic',
                          extra_compile_args=['-std=c++11', '-Werror'],
                          sources=[join('src', 'logistic', 'generated', name)
                                   for name in _le_srcs],
-                         **get_info("npymath"))
+                         **npymath_info)
 
     _yj_srcs = ['yeo_johnson_concrete.cxx', '_yeo_johnsonmodule.cxx']
     config.add_extension('ufunclab._yeo_johnson',
                          extra_compile_args=['-std=c++11', '-Werror'],
                          sources=[join('src', 'yeo_johnson', 'generated', name)
                                   for name in _yj_srcs],
-                         **get_info("npymath"))
+                         **npymath_info)
 
     _nm_srcs = ['normal_concrete.cxx', 'erfcx_funcs_concrete.cxx',
                 '_normalmodule.cxx']
@@ -113,48 +117,48 @@ def configuration(parent_package='', top_path=None):
                          sources=[join('src', 'normal', 'generated', name)
                                   for name in _nm_srcs] +
                                  [join('src', 'normal', 'erfcx_funcs.cxx')],
-                         **get_info("npymath"))
+                         **npymath_info)
 
     config.add_extension('ufunclab._cross',
                          extra_compile_args=compile_args,
                          sources=[join('src', 'cross',
                                        'cross_gufunc.c.src')],
-                         include_dirs=[join('src', 'util')])
+                         include_dirs=[util_include_dir])
 
     config.add_extension('ufunclab._peaktopeak',
                          extra_compile_args=compile_args,
                          sources=[join('src', 'peaktopeak',
                                        'peaktopeak_gufunc.c.src')],
-                         include_dirs=[join('src', 'util')])
+                         include_dirs=[util_include_dir])
 
     config.add_extension('ufunclab._first',
                          extra_compile_args=compile_args,
                          sources=[join('src', 'first',
                                        'first_gufunc.c.src')],
-                         include_dirs=[join('src', 'util')])
+                         include_dirs=[util_include_dir])
 
     config.add_extension('ufunclab._searchsorted',
                          extra_compile_args=compile_args,
                          sources=[join('src', 'searchsorted',
                                        'searchsorted_gufunc.c.src')],
-                         **get_info("npymath"))
+                         **npymath_info)
 
     config.add_extension('ufunclab._minmax',
                          extra_compile_args=compile_args,
                          sources=[join('src', 'minmax',
                                        'minmax_gufunc.c.src')],
-                         include_dirs=[join('src', 'util')])
+                         include_dirs=[util_include_dir])
 
     config.add_extension('ufunclab._means',
                          extra_compile_args=compile_args,
                          sources=[join('src', 'means', 'means_gufunc.c.src')],
-                         include_dirs=[join('src', 'util')])
+                         include_dirs=[util_include_dir])
 
     config.add_extension('ufunclab._meanvar',
                          extra_compile_args=compile_args,
                          sources=[join('src', 'meanvar',
                                        'meanvar_gufunc.c.src')],
-                         include_dirs=[join('src', 'util')])
+                         include_dirs=[util_include_dir])
 
     config.add_extension('ufunclab._mad',
                          extra_compile_args=compile_args,
@@ -165,26 +169,26 @@ def configuration(parent_package='', top_path=None):
                          extra_compile_args=['-std=c++11', '-Werror'],
                          sources=[join('src', 'vnorm', name)
                                   for name in _vnorm_srcs],
-                         include_dirs=[join('src', 'util')])
+                         **npymath_info)
 
     config.add_extension('ufunclab._backlash',
                          extra_compile_args=compile_args,
                          sources=[join('src', 'backlash',
                                        'backlash_gufunc.c.src')],
-                         include_dirs=[join('src', 'util')])
+                         include_dirs=[util_include_dir])
 
     config.add_extension('ufunclab._fillnan1d',
                          extra_compile_args=compile_args,
                          sources=[join('src', 'fillnan1d',
                                        'fillnan1d_gufunc.c.src')],
-                         include_dirs=[join('src', 'util')])
+                         include_dirs=[util_include_dir])
 
     _dz_srcs = ['deadzone_concrete.cxx', '_deadzonemodule.cxx']
     config.add_extension('ufunclab._deadzone',
                          extra_compile_args=['-std=c++11', '-Werror'],
                          sources=[join('src', 'deadzone', 'generated', name)
                                   for name in _dz_srcs],
-                         **get_info("npymath"))
+                         **npymath_info)
 
     _tp_srcs = ['trapezoid_pulse_concrete.cxx', '_trapezoid_pulsemodule.cxx']
     config.add_extension('ufunclab._trapezoid_pulse',
@@ -192,26 +196,26 @@ def configuration(parent_package='', top_path=None):
                          sources=[join('src', 'trapezoid_pulse', 'generated',
                                        name)
                                   for name in _tp_srcs],
-                         **get_info("npymath"))
+                         **npymath_info)
 
     config.add_extension('ufunclab._hysteresis_relay',
                          extra_compile_args=compile_args,
                          sources=[join('src', 'hysteresis_relay',
                                        'hysteresis_relay_gufunc.c.src')],
-                         include_dirs=[join('src', 'util')])
+                         include_dirs=[util_include_dir])
 
     config.add_extension('ufunclab._all_same',
                          extra_compile_args=compile_args,
                          sources=[join('src', 'all_same',
                                        'all_same_gufunc.c.src')],
-                         include_dirs=[join('src', 'util')])
+                         include_dirs=[util_include_dir])
 
     _step_srcs = ['step_funcs_concrete.cxx', '_stepmodule.cxx']
     config.add_extension('ufunclab._step',
                          extra_compile_args=['-std=c++11', '-Werror'],
                          sources=[join('src', 'step', 'generated', name)
                                   for name in _step_srcs],
-                         **get_info("npymath"))
+                         **npymath_info)
 
     config.add_extension('ufunclab._gendot',
                          extra_compile_args=compile_args,
