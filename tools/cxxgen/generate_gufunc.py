c544de2bbc44bc70df78401c0e09a3d844387fd4
Warren Weckesser <warren.weckesser@gmail.com>
2022-03-12 03:26:15 -0500

    Remove some print statements.

--- tools/cxxgen/generate_gufunc.py
+++ tools/cxxgen/generate_gufunc.py
@@ -4,18 +4,12 @@ from gufuncgen import gen
 
 
 def generate_gufunc(cxxgenpath):
-    print("generate_gufunc: cxxgenpath:", cxxgenpath)
     sys.path.append(cxxgendefpath)
     from define_cxx_gufunc_extmod import extmod
     sys.path.pop()
-    print("generate_gufunc: extmod.module:   ", extmod.module)
-    print("generate_gufunc: extmod.ufuncname:", extmod.ufuncname)
-    print("generate_gufunc: extmod.signature:", extmod.signature)
-    print("generate_gufunc: extmod.header:   ", extmod.header)
     del sys.modules['define_cxx_gufunc_extmod']
 
     module_name = os.path.join(cxxgenpath, extmod.module + 'module.cxx')
-    print('generate_gufunc: module_name:', module_name)
     text = gen(name=extmod.ufuncname,
                # varnames=varnames,
                signature=extmod.signature,
@@ -39,5 +33,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     cxxgendefpath = path.join(args.path)
-    print(f"****** Calling generate_gufunc({cxxgendefpath})")
     generate_gufunc(cxxgendefpath)
