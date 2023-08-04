d6767cb12ff0806d7d3b4fecb839a9855c19bc1a
warren <warren.weckesser@gmail.com>
2023-08-04 17:42:02 -0400

    New implementation of log1p(z)

--- src/log1p/log1p_ufunc.c
+++ src/log1p/log1p_ufunc.c
@@ -9,84 +9,13 @@
 #define PY_SSIZE_T_CLEAN
 #include "Python.h"
 
+#include <complex.h>
 #include <math.h>
 
 #define NPY_NO_DEPRECATED_API NPY_API_VERSION
 #include "numpy/ndarraytypes.h"
 #include "numpy/ufuncobject.h"
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Double-double functions used by log1p to avoid loss of precision
-// when the complex input z is close to the unit circle centered at -1+0j.
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-struct _doubledouble_t {
-    double upper;
-    double lower;
-};
-
-typedef struct _doubledouble_t doubledouble_t;
-
-static void
-two_sum_quick(double x, double y, doubledouble_t *out)
-{
-    double r = x + y;
-    double e = y - (r - x);
-    out->upper = r;
-    out->lower = e;
-}
-
-static void
-two_sum(double x, double y, doubledouble_t *out)
-{
-    double s = x + y;
-    double v = s - x;
-    double e = (x - (s - v)) + (y - v);
-    out->upper = s;
-    out->lower = e;
-}
-
-static void
-double_sum(const doubledouble_t x, const doubledouble_t y,
-           doubledouble_t *out)
-{
-    two_sum(x.upper, y.upper, out);
-    out->lower += x.lower + y.lower;
-    two_sum_quick(out->upper, out->lower, out);
-}
-
-static void
-split(double x, doubledouble_t *out)
-{
-    double t = ((1 << 27) + 1)*x;
-    out->upper = t - (t - x);
-    out->lower = x - out->upper;
-}
-
-static void
-square(double x, doubledouble_t *out)
-{
-    doubledouble_t xsplit;
-    out->upper = x*x;
-    split(x, &xsplit);
-    out->lower = (xsplit.upper*xsplit.upper - out->upper)
-                  + 2*xsplit.upper*xsplit.lower
-                  + xsplit.lower*xsplit.lower;
-}
-
-static double
-foo(double x, double y)
-{
-    doubledouble_t x2, y2, twox, sum1, sum2;
-
-    square(x, &x2);
-    square(y, &y2);
-    twox.upper = 2*x;
-    twox.lower = 0.0;
-    double_sum(x2, twox, &sum1);
-    double_sum(sum1, y2, &sum2);
-    return sum2.upper;
-}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // The ufunc "inner loop". The type signatures of the log1p ufunc are
@@ -94,8 +23,9 @@ foo(double x, double y)
 //     ['D->D']
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-static void log1p_D_D_loop(char **args, const npy_intp *dimensions,
-                           const npy_intp* steps, void* data)
+static void
+log1p_D_D_loop(char **args, const npy_intp *dimensions,
+               const npy_intp* steps, void* data)
 {
     char *in = args[0];
     char *out = args[1];
@@ -103,27 +33,29 @@ static void log1p_D_D_loop(char **args, const npy_intp *dimensions,
     npy_intp out_step = steps[1];
 
     for (npy_intp i = 0; i < dimensions[0]; ++i, in += in_step, out += out_step) {
-        double lnr;
-        double x = (double) *(double *)in;
-        double y = (double) *(double *)(in + sizeof(double));
+        double _Complex w;
+        double _Complex z = *(double _Complex *) in;
 
-        if (isnan(x) || isnan(y)) {
-            *((double *) out) = NAN;
-            *((double *) (out + sizeof(double))) = NAN;            
+        if (isnan(creal(z)) || isnan(cimag(z))) {
+            w = CMPLX(NAN, NAN);
         }
         else {
-            if (x > -2.2 && x < 0.2 && y > -1.2 && y < 1.2
-                    && fabs(x*(2.0 + x) + y*y) < 0.4) {
-                // The input is close to the unit circle centered at -1+0j.
-                // Use double-double to evaluate the real part of the result.
-                lnr = 0.5*log1p(foo(x, y));
+            double _Complex u = z + 1.0;
+            if (creal(u) == 1.0 && cimag(u) == 0.0) {
+                // z + 1 == 1
+                w = z;
             }
             else {
-                lnr = log(hypot(x + 1, y));
+                if (creal(u) - 1.0 == creal(z)) {
+                    // u - 1 == z
+                    w = clog(u);
+                }
+                else {
+                    w = clog(u) * (z / (u - 1.0));
+                }
             }
-            *((double *) out) = lnr;
-            *((double *) (out + sizeof(double))) = atan2(y, x + 1.0);
         }
+        *((double _Complex *) out) = w;
     }
 }
 
--- ufunclab/tests/test_log1p.py
+++ ufunclab/tests/test_log1p.py
@@ -11,10 +11,17 @@ from ufunclab import log1p
      (-0.57113 - 0.90337j, 3.4168883248419116e-06 - 1.1275564209486122j),
      (-0.978249978133276 - 0.015379941691430407j,
       -3.6254002951579887 - 0.6154905511361795j),
+     (-0.011228922063957758 + 0.14943813247359922j,
+      -4.499779370610757e-17 + 0.15j),
+     (-1.9999999995065196 - 3.141592653092555e-05j,
+      -1.4766035946815242e-16 - 3.1415612376632573j),
+     (-1.25e-8 + 5e-12j, -1.2500000078124988e-08 + 5.0000000625e-12j),
      (1e200 + 1e200j, 460.8635921890891 + 0.7853981633974483j)],
 )
 def test_basic(z, wref):
     w = log1p(z)
+    # Note that we test the relative error of the real and imaginary parts
+    # separately.  This is a more stringent test than abs(w - wref)/abs(wref).
     assert_allclose(w.real, wref.real, rtol=1e-15)
     assert_allclose(w.imag, wref.imag, rtol=1e-15)
 
@@ -25,3 +32,20 @@ def test_basic(z, wref):
 def test_tiny(z):
     w = log1p(z)
     assert w == z
+
+
+# These are values where real part is not computed with
+# extremely high precision.
+@pytest.mark.parametrize(
+    'z, wref',
+    [(-1.25e-05 + 0.005j, 7.812499999381789e-11 + 0.005000020833177082j),
+     (-8e-8 - 0.0004j, 3.2000000031864308e-15 - 0.00040000001066666614j),
+     (-0.01524813-0.173952j, -2.228118716056777e-06-0.1748418364650139j)],
+)
+def test_real_part_loosely(z, wref):
+    w = log1p(z)
+    # The computed value is very close to the reference value...
+    assert_allclose(w, wref, rtol=1e-15)
+    # ...but the real parts (which are very small) have greater relative
+    # error...
+    assert_allclose(w.real, wref.real, rtol=5e-9)
