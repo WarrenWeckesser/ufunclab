7637521c41e29ca01e3acc4f9166ea483fc22ebc
warren <warren.weckesser@gmail.com>
2023-09-07 20:45:03 -0400

    TST: Skip abs_squared test for longdouble when it is the same as float64

--- ufunclab/tests/test_abs_squared.py
+++ ufunclab/tests/test_abs_squared.py
@@ -7,6 +7,8 @@ from ufunclab import abs_squared, cabssq
 @pytest.mark.parametrize('dtype',
                          [np.float32, np.float64, np.longdouble])
 def test_basic_real(dtype):
+    if dtype == np.longdouble and np.dtype('g') == np.dtype('d'):
+        pytest.skip('longdouble is double')
     x = np.array([-1.0, 2.5, 9.0, 125.0], dtype=dtype)
     y = abs_squared(x)
     expected = x**2
@@ -18,6 +20,8 @@ def test_basic_real(dtype):
 @pytest.mark.parametrize('dtype',
                          [np.complex64, np.complex128, np.clongdouble])
 def test_basic_complex(func, dtype):
+    if dtype == np.clongdouble and np.dtype('g') == np.dtype('d'):
+        pytest.skip('longdouble is double')
     z = np.array([-1-1j, 2.5j, 9.0-3j, 125.0, -8.0], dtype=dtype)
     y = func(z)
     expected = z.real**2 + z.imag**2
