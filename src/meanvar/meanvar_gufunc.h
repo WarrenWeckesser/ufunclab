0158e4e451a87899d211b98924151da201995b53
Warren Weckesser <warren.weckesser@gmail.com>
2025-02-18 12:51:26 -0500

    MAINT: Another tweak of the c++ code for meanvar.

--- src/meanvar/meanvar_gufunc.h
+++ src/meanvar/meanvar_gufunc.h
@@ -4,11 +4,24 @@
 #define PY_SSIZE_T_CLEAN
 #include "Python.h"
 
+#include <utility>
+#include <tuple>
+
 #define NPY_NO_DEPRECATED_API NPY_API_VERSION
 #include "numpy/ndarraytypes.h"
 
 #include "../src/util/strided.hpp"
 
+
+template<typename U>
+inline std::pair<U, U>
+compensated_update(U val, U incr, U comp)
+{
+    U y = incr - comp;
+    U t = val + y;
+    U c = (t - val) - y;
+    return std::make_pair(t, c);
+}
 
 template<typename T, typename U>
 static void meanvar_core(
@@ -18,27 +31,18 @@ static void meanvar_core(
         npy_intp *p_ddof,     // pointer to ddof
         U *p_out,             // pointer to first element of out, a strided 1-d array with 2 elements
         npy_intp out_stride   // stride (in bytes) for elements of out
-) {
+)
+{
     U mean = 0.0;
-    U var = 0.0;
-    U c1 = 0.0, m2 = 0.0, c2 = 0.0;
+    U m2 = 0.0;
+    U c1 = 0.0, c2 = 0.0;
     for (npy_intp k = 0; k < n; ++k) {
         T xk = get(p_x, x_stride, k);
         U delta = xk - mean;
-        {
-            U y1 = delta/(k + 1) - c1;
-            U t1 = mean + y1;
-            c1 = (t1 - mean) - y1;
-            mean = t1;
-        }
-        {
-            U y2 = delta * (xk - mean) - c2;
-            U t2 = m2 + y2;
-            c2 = (t2 - m2) - y2;
-            m2 = t2;
-        }
+        std::tie(mean, c1) = compensated_update(mean, delta/(k + 1), c1);
+        std::tie(m2, c2) = compensated_update(m2, delta*(xk - mean), c2);
     }
-    var = m2 / (n - *p_ddof);
+    U var = m2 / (n - *p_ddof);
     *p_out = mean;
     set(p_out, out_stride, 1, var);
 }
