bb79baa0f2422ce084a9918decc75608891a5750
DWesl <22566757+DWesl@users.noreply.github.com>
2026-05-28 18:48:31 -0400

    PERF: Convert two-pass mean-variance algorithm to use pairwise summation.
    Currently uses a fixed ten-bin accumulator for the pairwise summation,
    which can handle batches of 512 values before dumping the batch sum in the running sum.
    
    I'm realizing the axes on the covariance are backwards from np.cov.
    I should maybe fix that.

--- src/meanvar/meanvar_gufunc.h
+++ src/meanvar/meanvar_gufunc.h
@@ -23,6 +23,51 @@ compensated_update(U val, U incr, U comp)
     return std::make_pair(t, c);
 }
 
+template <typename Real, size_t N_accumulators=10>
+class pairwise_accumulator {
+private:
+  Real accumulators[N_accumulators] = {0};
+  std::size_t n_elements = 0, accum_index = 0;
+
+public:
+  inline void reset() {
+    for (size_t i = 0; i < N_accumulators; ++i) {
+      accumulators[i] = 0;
+    }
+    n_elements = 0;
+    accum_index = 0;
+  }
+
+  inline void add_data(Real num) {
+    ++n_elements;
+    accumulators[accum_index] += num;
+    for (std::size_t tst = 1;
+	 tst < n_elements && (n_elements & tst) == 0 && accum_index > 0;
+	 tst <<= 1, --accum_index) {
+      accumulators[accum_index - 1] += accumulators[accum_index];
+      accumulators[accum_index] = 0;
+    }
+    ++accum_index;
+    if (accum_index == N_accumulators) {
+      Real tmp = 0;
+      for (size_t i = N_accumulators - 1; i > 0; --i) {
+	tmp += accumulators[i];
+	accumulators[i] = 0;
+      }
+      accumulators[0] += tmp;
+      accum_index = 1;
+    }
+  }
+
+  constexpr inline Real get_sum() {
+    Real result = 0;
+    for (long i = N_accumulators - 1; i >= 0; --i) {
+      result += accumulators[i];
+    }
+    return result;
+  }
+};
+
 template<typename T, typename U>
 static void meanvar_core(
         npy_intp n,           // core dimension n
@@ -50,26 +95,28 @@ static void meanvar_core(
 template<typename T, typename U>
 static void meanvar_twopass_core(
         npy_intp n,           // core dimension n
-        const T * const p_x,               // pointer to first element of x, a strided 1-d array with n elements
+        const T * const p_x,          // pointer to first element of x, a strided 1-d array with n elements
         npy_intp x_stride,    // stride (in bytes) for elements of x
-        const npy_intp * const p_ddof,     // pointer to ddof
+        const npy_intp * const p_ddof,   // pointer to ddof
         U *p_out,             // pointer to first element of out, a strided 1-d array with 2 elements
         npy_intp out_stride   // stride (in bytes) for elements of out
 )
 {
+  pairwise_accumulator<U, 10> accumulator;
     U mean = 0.0;
     U var = 0.0, tmp;
     for (npy_intp k = 0; k < n; ++k) {
-        T xk = get(p_x, x_stride, k);
-	mean += xk;
+      T xk = get(p_x, x_stride, k);
+      accumulator.add_data(xk);
     }
-    mean /= n;
+    mean = accumulator.get_sum() / n;
+    accumulator.reset();
     for (npy_intp k = 0; k < n; ++k) {
       T xk = get(p_x, x_stride, k);
       tmp = xk - mean;
-      var += tmp * tmp;
+      accumulator.add_data(tmp * tmp);
     }
-    var /= (n - *p_ddof);
+    var = accumulator.get_sum() / (n - *p_ddof);
     *p_out = mean;
     set(p_out, out_stride, 1, var);
 }
@@ -86,17 +133,17 @@ static void covariance_matrix_core(
 				   )
 {
   U mean[m] = {0.0};
-  U cov[m*m] = {0.0};
+  pairwise_accumulator<U, 10> mean_accum[m], cov_accum[m*m];
   U tmp1, tmp2;
   for (npy_intp i = 0; i < n; ++i) {
     #pragma GCC ivdep
     for (npy_intp j = 0; j < m; ++j) {
       T x_ij = get2d(p_x, x_strides, i, j);
-      mean[j] += x_ij;
+      mean_accum[j].add_data(x_ij);
     }
   }
   for (npy_intp j = 0; j < m; ++j) {
-    mean[j] /= n;
+    mean[j] = mean_accum[j].get_sum() / n;
   }
   for (npy_intp i = 0; i < n; ++i) {
     #pragma GCC ivdep
@@ -107,13 +154,13 @@ static void covariance_matrix_core(
       for (npy_intp k = j; k < m; ++k) {
 	T x_ik = get2d(p_x, x_strides, i, k);
 	tmp2 = x_ik - mean[k];
-	cov[j * m + k] += tmp1 * tmp2;
+	cov_accum[j * m + k].add_data(tmp1 * tmp2);
       }
     }
   }
   for (npy_intp j = 0; j < m; ++j) {
     for (npy_intp k = j; k < m; ++k) {
-      tmp1 = cov[j * m + k] / (n - *p_ddof);
+      tmp1 = cov_accum[j * m + k].get_sum() / (n - *p_ddof);
       set2d(p_out, out_strides, j, k, tmp1);
       set2d(p_out, out_strides, k, j, tmp1);
     }
